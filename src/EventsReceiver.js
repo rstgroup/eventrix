7c6bfb6b5a6dab90fb8f63a21ded63db09bc2939
Mariusz Przodała <przodala@gmail.com>
2020-11-03 21:10:41 +0100

    fetchHandler

--- src/EventsReceiver.js
+++ src/EventsReceiver.js
@@ -11,10 +11,27 @@ class EventsReceiver {
     }
 }
 
+export const fetchHandler = (fetchMethod, { success, error }) => {
+    return (eventData, state, emit) =>
+        fetchMethod(eventData, state, emit)
+            .then((response) => {
+                const { eventName, data, getData } = success;
+                const successEventData = (getData && typeof getData === 'function') ? getData(response, eventData) : data;
+                emit(eventName, successEventData);
+                return response;
+            })
+            .catch((errorResponse) => {
+                const { eventName, data, getData } = error;
+                const errorEventData = (getData && typeof getData === 'function') ? getData(errorResponse, eventData) : data;
+                emit(eventName, errorEventData);
+            });
+};
+
 export const fetchToStateReceiver = (eventName, statePath, fetchMethod) => {
     return new EventsReceiver(eventName, (name, eventData, stateManager) => {
         const state = stateManager.getState();
-        return fetchMethod(eventData, state).then((nextState) => {
+        const emit = stateManager.eventsEmitter.emit;
+        return fetchMethod(eventData, state, emit).then((nextState) => {
             if (nextState !== undefined) {
                 const path =
                     typeof statePath === "function"
--- src/EventsReceiver.test.js
+++ src/EventsReceiver.test.js
@@ -1,4 +1,4 @@
-import EventsReceiver, { fetchToStateReceiver } from './EventsReceiver';
+import EventsReceiver, { fetchHandler, fetchToStateReceiver } from './EventsReceiver';
 
 describe('EventsReceiver', () => {
     it('should get receiver events names when events names are string', () => {
@@ -18,13 +18,20 @@ describe('EventsReceiver', () => {
 });
 
 describe('fetchToStateReceiver', () => {
+    let stateManager = {};
+    beforeEach(() => {
+        stateManager = {
+            setState: jest.fn(),
+            getState: jest.fn(() => ({})),
+            eventsEmitter: {
+                emit: jest.fn(),
+            }
+        };
+    });
     it('should handle event and set state by stateManager when promise resolved', () => {
         const receiverResponse = { test: 'test' };
         const receiver = () => Promise.resolve(receiverResponse);
-        const stateManager = {
-            setState: jest.fn(),
-            getState: jest.fn(() => ({})),
-        };
+
         const eventsReceiver = fetchToStateReceiver('testEvent', 'testStateName', receiver);
         return eventsReceiver.handleEvent('testEvent', {}, stateManager).then(() => {
             expect(stateManager.setState).toHaveBeenCalledWith('testStateName', receiverResponse);
@@ -33,10 +40,7 @@ describe('fetchToStateReceiver', () => {
     it('should handle event and set state by stateManager when promise resolved and stateName is function', () => {
         const receiverResponse = { test: 'test' };
         const receiver = () => Promise.resolve(receiverResponse);
-        const stateManager = {
-            setState: jest.fn(),
-            getState: jest.fn(() => ({})),
-        };
+
         const eventsReceiver = fetchToStateReceiver('testEvent', ({ prefix }) => `${prefix}testStateName`, receiver);
         return eventsReceiver.handleEvent('testEvent', { prefix: 'test.' }, stateManager).then(() => {
             expect(stateManager.setState).toHaveBeenCalledWith('test.testStateName', receiverResponse);
@@ -45,10 +49,7 @@ describe('fetchToStateReceiver', () => {
 
     it('should handle event and not set state when promise rejected', () => {
         const receiver = () => Promise.reject('error');
-        const stateManager = {
-            setState: jest.fn(),
-            getState: jest.fn(() => ({})),
-        };
+
         const eventsReceiver = fetchToStateReceiver('testEvent', 'testStateName', receiver);
         return eventsReceiver.handleEvent('testEvent', {}, stateManager).catch(() => {
             expect(stateManager.setState).not.toHaveBeenCalled();
@@ -57,13 +58,119 @@ describe('fetchToStateReceiver', () => {
 
     it('should handle event and not set state when promise rejected and has catch', () => {
         const receiver = () => Promise.reject('error').catch(() => {});
-        const stateManager = {
-            setState: jest.fn(),
-            getState: jest.fn(() => ({})),
-        };
+
         const eventsReceiver = fetchToStateReceiver('testEvent', 'testStateName', receiver);
         return eventsReceiver.handleEvent('testEvent', {}, stateManager).then(() => {
             expect(stateManager.setState).not.toHaveBeenCalled();
         });
+    });
+});
+
+describe('fetchHandler', () => {
+    let emit;
+    beforeEach(() => {
+        emit = jest.fn();
+    });
+    it('should fetch data and emit success event when promise resolved', () => {
+        const fetchResponse = { test: 'test' };
+        const fetchMethod = () => Promise.resolve(fetchResponse);
+        const successEventName = 'fetch.success';
+        const successEventData = 'test success';
+        const errorEventName = 'fetch.error';
+        const errorEventData = 'test error';
+        const fetchMethodWithHandler = fetchHandler(
+            fetchMethod,
+            {
+                success: {
+                    eventName: successEventName,
+                    data: successEventData
+                },
+                error: {
+                    eventName: errorEventName,
+                    data: errorEventData
+                }
+            }
+        );
+        return fetchMethodWithHandler({}, '', emit).then((response) => {
+            expect(response).toEqual(fetchResponse);
+            expect(emit).toHaveBeenCalledWith(successEventName, successEventData);
+            expect(emit).not.toHaveBeenCalledWith(errorEventName, errorEventData);
+        })
+    });
+    it('should fetch data and emit success event with response in event data', () => {
+        const fetchResponse = { test: 'test' };
+        const fetchMethod = () => Promise.resolve(fetchResponse);
+        const successEventName = 'fetch.success';
+        const successGetEventData = (response) => ({ response , message: 'test success' });
+        const errorEventName = 'fetch.error';
+        const errorGetEventData = (errorResponse) => ({ errorResponse, message: 'test error' });
+        const fetchMethodWithHandler = fetchHandler(
+            fetchMethod,
+            {
+                success: {
+                    eventName: successEventName,
+                    getData: successGetEventData
+                },
+                error: {
+                    eventName: errorEventName,
+                    getData: errorGetEventData
+                }
+            }
+        );
+        return fetchMethodWithHandler({}, '', emit).then((response) => {
+            expect(response).toEqual(fetchResponse);
+            expect(emit).toHaveBeenCalledWith(successEventName, { response , message: 'test success' });
+        })
+    });
+    it('should fetch data and emit error event when promise rejected', () => {
+        const fetchResponse = { test: 'test' };
+        const fetchMethod = () => Promise.reject(fetchResponse);
+        const successEventName = 'fetch.success';
+        const successEventData = 'test success';
+        const errorEventName = 'fetch.error';
+        const errorEventData = 'test error';
+        const fetchMethodWithHandler = fetchHandler(
+            fetchMethod,
+            {
+                success: {
+                    eventName: successEventName,
+                    data: successEventData
+                },
+                error: {
+                    eventName: errorEventName,
+                    data: errorEventData
+                }
+            }
+        );
+        return fetchMethodWithHandler({}, '', emit).then((response) => {
+            expect(response).toEqual(undefined);
+            expect(emit).not.toHaveBeenCalledWith(successEventName, successEventData);
+            expect(emit).toHaveBeenCalledWith(errorEventName, errorEventData);
+        })
+    });
+    it('should fetch data and emit error event with error response in event data', () => {
+        const errorResponse = { test: 'test' };
+        const fetchMethod = () => Promise.reject(errorResponse);
+        const successEventName = 'fetch.success';
+        const successGetEventData = (response) => ({ response , message: 'test success' });
+        const errorEventName = 'fetch.error';
+        const errorGetEventData = (response) => ({ response, message: 'test error' });
+        const fetchMethodWithHandler = fetchHandler(
+            fetchMethod,
+            {
+                success: {
+                    eventName: successEventName,
+                    getData: successGetEventData
+                },
+                error: {
+                    eventName: errorEventName,
+                    getData: errorGetEventData
+                }
+            }
+        );
+        return fetchMethodWithHandler({}, '', emit).then((response) => {
+            expect(response).toEqual(undefined);
+            expect(emit).toHaveBeenCalledWith(errorEventName, { response: errorResponse, message: 'test error' });
+        })
     });
 });
