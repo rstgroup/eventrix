184d747a7807f655f5df8265f22d6e01e8d28c73
Mariusz Przodała <przodala@gmail.com>
2021-06-27 19:18:02 +0200

    merge

---
+++ src/redux/combineReducers.ts
@@ -0,0 +1,20 @@
+import EventsReceiver from "../EventsReceiver";
+import { DISPATCH_EVENT_NAME } from "./events";
+import {ActionI, ReducersI, StateManagerI} from "../interfaces";
+
+
+const combineReducers = (reducers: ReducersI) => {
+    return [
+        new EventsReceiver(DISPATCH_EVENT_NAME, (eventName: string, action: ActionI, stateManager: StateManagerI): void => {
+            Object.keys(reducers).forEach((key: string) => {
+                const state = stateManager.getState(key);
+                const stateFromReducer = reducers[key](state, action);
+                if (state !== stateFromReducer) {
+                    stateManager.setState(key, stateFromReducer);
+                }
+            });
+        })
+    ];
+};
+
+export default combineReducers;
---
+++ src/redux/events.ts
@@ -0,0 +1,1 @@
+export const DISPATCH_EVENT_NAME = 'Redux:dispatch';
