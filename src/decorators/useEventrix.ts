be12a83095b9b750703dc4f79df1dd00a707d74c
Alan Cesarski <alan@cesarski.dev>
2023-10-12 13:05:24 +0200

    feat: add ability to specify key of eventrix service

--- src/decorators/useEventrix.ts
+++ src/decorators/useEventrix.ts
@@ -31,33 +31,35 @@ export interface EventrixClassI {
     [key: string]: any;
 }
 
-function useEventrix(targetClass: any) {
-    const original: EventrixClassI = targetClass;
-    const eventrixClass: any = function (services: ServicesI, ...args: any[]) {
-        const classInstance = new original(services, ...args);
-        classInstance.eventrix = services.eventrix;
-        if (Array.isArray(classInstance.eventrixReceivers)) {
-            classInstance.eventrixReceivers.forEach(({ eventsNames, name }) => {
-                classInstance[name] = classInstance[name].bind(classInstance);
-                classInstance.eventrix!.useReceiver(new EventsReceiver(eventsNames, classInstance[name]));
-            });
-        }
-        if (Array.isArray(classInstance.eventrixFetchToStateReceivers)) {
-            classInstance.eventrixFetchToStateReceivers.forEach(({ eventName, statePath, name }) => {
-                classInstance[name] = classInstance[name].bind(classInstance);
-                classInstance.eventrix!.useReceiver(fetchToStateReceiver(eventName, statePath, classInstance[name]));
-            });
-        }
-        if (Array.isArray(classInstance.eventrixListeners)) {
-            classInstance.eventrixListeners.forEach(({ eventName, name }) => {
-                classInstance[name] = classInstance[name].bind(classInstance);
-                classInstance.eventrix!.listen(eventName, classInstance[name]);
-            });
-        }
-        return classInstance;
+function useEventrix(eventrixServiceKey = 'eventrix') {
+    return function (targetClass: any) {
+        const original: EventrixClassI = targetClass;
+        const eventrixClass: any = function (services: ServicesI, ...args: any[]) {
+            const classInstance = new original(services, ...args);
+            classInstance.eventrix = services[eventrixServiceKey];
+            if (Array.isArray(classInstance.eventrixReceivers)) {
+                classInstance.eventrixReceivers.forEach(({ eventsNames, name }) => {
+                    classInstance[name] = classInstance[name].bind(classInstance);
+                    classInstance.eventrix!.useReceiver(new EventsReceiver(eventsNames, classInstance[name]));
+                });
+            }
+            if (Array.isArray(classInstance.eventrixFetchToStateReceivers)) {
+                classInstance.eventrixFetchToStateReceivers.forEach(({ eventName, statePath, name }) => {
+                    classInstance[name] = classInstance[name].bind(classInstance);
+                    classInstance.eventrix!.useReceiver(fetchToStateReceiver(eventName, statePath, classInstance[name]));
+                });
+            }
+            if (Array.isArray(classInstance.eventrixListeners)) {
+                classInstance.eventrixListeners.forEach(({ eventName, name }) => {
+                    classInstance[name] = classInstance[name].bind(classInstance);
+                    classInstance.eventrix!.listen(eventName, classInstance[name]);
+                });
+            }
+            return classInstance;
+        };
+        eventrixClass.prototype = original.prototype;
+        return eventrixClass;
     };
-    eventrixClass.prototype = original.prototype;
-    return eventrixClass;
 }
 
 export default useEventrix;
