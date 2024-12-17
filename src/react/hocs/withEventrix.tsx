4fd36b362e60aa2f6695272dd85838cfa3d62c89
JUtupe <jutuper@gmail.com>
2024-12-17 19:00:27 +0100

    feat: withEventrix ref forwarding

--- src/react/hocs/withEventrix.tsx
+++ src/react/hocs/withEventrix.tsx
@@ -1,13 +1,15 @@
-import React, { useContext } from 'react';
+import React, { forwardRef, ForwardRefExoticComponent, PropsWithoutRef, ReactNode, RefAttributes, useContext } from 'react';
 import { EventrixContext } from '../context';
 import { EventrixI } from '../../interfaces';
 
-function withEventrix<PropsI>(BaseComponent: React.ComponentType<PropsI> | React.FC<PropsI>): React.FC<PropsI> {
-     
-    return (props: PropsI & { eventrix: EventrixI }): JSX.Element => {
+function withEventrix<PropsI, R = unknown>(
+    BaseComponent: React.ComponentType<PropsI & { eventrix: EventrixI }>,
+): ForwardRefExoticComponent<PropsWithoutRef<PropsI> & RefAttributes<R>> {
+    return forwardRef<R, PropsI>((props: PropsWithoutRef<PropsI>, ref): ReactNode => {
         const context = useContext(EventrixContext);
-        return <BaseComponent {...props} eventrix={context.eventrix} />;
-    };
+        // @ts-ignore
+        return <BaseComponent {...props} ref={ref} eventrix={context.eventrix} />;
+    });
 }
 
 export default withEventrix;
